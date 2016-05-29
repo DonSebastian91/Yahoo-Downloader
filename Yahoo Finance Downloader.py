de55a278cc01c55280d29072185c89f3e19e36cf
jack6347 <SebastianLehner@gmx.de>
2016-05-29 14:15:45 +0200

    Update Yahoo Finance Downloader.py

--- Yahoo Finance Downloader.py
+++ Yahoo Finance Downloader.py
@@ -3,6 +3,7 @@ import pandas as pd
 from pandas_datareader import data, wb
 import datetime as dt
 
+# Get dates and make sure datetime can use them
 date_start = input('Enter a start date (YYYY-MM-DD): ')
 year, month, day = map(int, date_start.split('-'))
 start = dt.date(year, month, day)
@@ -11,6 +12,8 @@ date_end = input('Enter an end date (YYYY-MM-DD): ')
 year, month, day = map(int, date_end.split('-'))
 end = dt.date(year, month, day)
 
+# Get yahoo finance data
 df = data.DataReader("AAPL", 'yahoo', start, end)
 
+# Convert to CSV
 df.to_csv('Yahoo_Data')
