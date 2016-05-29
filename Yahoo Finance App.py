83d927afbf8ee3ae3a52f3008bdf25d3128c03a0
jack6347 <SebastianLehner@gmx.de>
2016-05-29 14:13:40 +0200

    Update Yahoo Finance App.py

--- Yahoo Finance App.py
+++ Yahoo Finance App.py
@@ -1,15 +1,7 @@
-
-# coding: utf-8
-
-# In[1]:
-
 import numpy as np
 import pandas as pd
 from pandas_datareader import data, wb
 import datetime as dt
-
-
-# In[2]:
 
 date_start = input('Enter a start date (YYYY-MM-DD): ')
 year, month, day = map(int, date_start.split('-'))
@@ -19,13 +11,6 @@ date_end = input('Enter an end date (YYYY-MM-DD): ')
 year, month, day = map(int, date_end.split('-'))
 end = dt.date(year, month, day)
 
-
-# In[3]:
-
 df = data.DataReader("AAPL", 'yahoo', start, end)
 
-
-# In[4]:
-
 df.to_csv('Yahoo_Data')
-
